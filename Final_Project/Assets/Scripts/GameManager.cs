b9ab4cfcb5b6b82933b3a12719ae2c554e6cc194
Billy <billyanthonyvilla@gmail.com>
2019-02-21 11:43:46 -0800

    Added desert tilemaps, objects, player, background, environment, and scripts, gamemanager camera pause

---
+++ Final_Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    bool isPause = false;
+    string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneName == "MainMenu")
+        {
+            PlayGame();
+        }
+
+        if(sceneName != "MainMenu" && Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    void PauseGame()
+    {
+        isPause = !isPause;
+        if (isPause)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+}
---
+++ Final_Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    bool isPause = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        isPause = !isPause;
+        if(isPause)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
