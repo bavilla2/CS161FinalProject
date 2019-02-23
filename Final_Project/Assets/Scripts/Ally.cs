24dc2f8b78e98fecabd550b453cbc8b81a24e74a
Chris Bond <chrisbondf@dhcp-v082-030.mobile.uci.edu>
2019-02-23 14:29:11 -0800

    Ally Script (added to each prefab)

---
+++ Final_Project/Assets/Scripts/Ally.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ally : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            if (tag == "Popsicle")
+            {
+                Debug.Log("10 s");
+                GameObject.Find("Player").GetComponent<Player>().health += 10.0f;
+                Destroy(this.gameObject);
+            }
+            else if (tag == "AC")
+            {
+                Debug.Log("30 s");
+                GameObject.Find("Player").GetComponent<Player>().health += 30.0f;
+                Destroy(this.gameObject);
+            }
+            else if (tag == "IceCube")
+            {
+                Debug.Log("5 s");
+                GameObject.Find("Player").GetComponent<Player>().health += 5.0f;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
