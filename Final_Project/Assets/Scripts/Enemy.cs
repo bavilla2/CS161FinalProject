3f70df316a102d9fc34f7e57c458baf60021fdd8
Avineesh Kompella <AvineeshKom@gmail.com>
2019-02-22 16:46:19 -0800

    Enemy stuff

---
+++ Final_Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (tag == "Grill")
+            {
+                Debug.Log("10 s");
+                player.GetComponent<Player>().health -= 10;
+            }
+            else if (tag == "Flamethrower")
+            {
+                Debug.Log("30 s");
+                player.GetComponent<Player>().health -= 30;
+            }
+            else if (collision.CompareTag("Fire"))
+            {
+                Debug.Log("5 s");
+                player.GetComponent<Player>().health -= 5;
+            }
+        }
+    }
+}
