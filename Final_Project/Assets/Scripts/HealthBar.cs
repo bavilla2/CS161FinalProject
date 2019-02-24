8f6bd088b1f62aac68e7655c4906473683122bcf
Julian Sollozo <jsollozo@uci.edu>
2019-02-23 23:15:57 -0800

    Health Bar Implemented
    -Added Healthbar Prefab
    -Small updates to my Scene

---
+++ Final_Project/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class HealthBar : MonoBehaviour
+{
+    TextMeshProUGUI Health_Text;
+    float Player_Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health_Text = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player_Health = GameObject.Find("Player").GetComponent<Player>().health;
+        Health_Text.text = string.Format("Health {0:0}", Player_Health);
+    }
+}
