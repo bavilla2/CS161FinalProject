3e8398b14b5afa09cc13ec6dbeb18a5d135df675
Billy <billyanthonyvilla@gmail.com>
2019-02-28 20:31:02 -0800

    Added lava_palate, fixed up prefabs by deleting enemy and ally script, and fixed in jump player

--- Final_Project/Assets/Scripts/Player.cs
+++ Final_Project/Assets/Scripts/Player.cs
@@ -6,13 +6,14 @@ public class Player : MonoBehaviour
 {
     [SerializeField] public float speed = 1;
     private Rigidbody2D m_rigidbody;
-    [SerializeField] public float jumpForce = 7.5f;
-    [SerializeField] public float health = 100.00f;
+    [SerializeField] public float jumpForce = 0f;//= 7.5f;
+    [SerializeField] public float health = 50.00f;
     private bool isGrounded = false;
     private bool alive = true;
     private Collider2D m_collider;
     [SerializeField] public bool isThrown = false;
-    public Animator animator; 
+    public Animator animator;
+    private RaycastHit2D jumpInfo; 
 
 
     public GameObject snowball;
@@ -26,6 +27,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector2 feetPosition = new Vector2(this.transform.position.x, m_collider.bounds.min.y);
+        jumpInfo = Physics2D.Raycast(feetPosition, Vector2.down, 1f);
+        Debug.DrawRay(feetPosition, Vector2.down * 1f, Color.green);
+
         life();
         if(!alive)
         {
@@ -41,17 +47,26 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            jumpForce = 9f;
             Jump();
+        }
+
+        if(!isGrounded)
+        {
+            if(jumpForce >= 0.01f)
+            {
+                jumpForce -= 2.0f;
+            }
+            else {
+                jumpForce = 0.0f;
+            }
         }
 
         //Debug.Log(health);
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            print("I pressed F");
             StartCoroutine(Throw_Snowball_Animation());
-            //throw_snowball();
-            //animator.SetBool("Is_thrown", false);
         }
 
     }
@@ -79,17 +94,28 @@ public class Player : MonoBehaviour
         m_rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (jumpInfo && jumpInfo.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
             isGrounded = true;
 
-            Vector2 feetPosition = new Vector2(this.transform.position.x, m_collider.bounds.min.y);
-            RaycastHit2D jumpInfo = Physics2D.Raycast(feetPosition, Vector2.down, 0.1f);
-            Debug.DrawRay(feetPosition, Vector2.down * 0.1f, Color.green);
+            /*Vector2 feetPosition = new Vector2(this.transform.position.x, m_collider.bounds.min.y);
+            //RaycastHit2D jumpInfo = Physics2D.Raycast(feetPosition, Vector2.down, 0.1f);
+            //Debug.DrawRay(feetPosition, Vector2.down * 0.1f, Color.green);
+            RaycastHit2D jumpInfo = Physics2D.Raycast(feetPosition, Vector2.down, 0.6f);
+            Debug.DrawRay(feetPosition, Vector2.down * 0.6f, Color.green);*/
             if (jumpInfo && jumpInfo.collider.CompareTag("Ground"))
             {
+                Debug.Log("I can jump again");
                 isGrounded = true;
             }
         }
@@ -111,42 +137,35 @@ public class Player : MonoBehaviour
         }
         else if(collider.CompareTag("Cactus"))
         {
-            Debug.Log("5 s");
             health -= 5;
         }
         else if (collider.CompareTag("Fire"))
         {
-            Debug.Log("20 s");
-            health -= 20;
+            health -= 15;
             Destroy(collider.gameObject);
         }
         else if (collider.CompareTag("Flamethrower"))
         {
-            Debug.Log("30 s");
-            health -= 30;
+            health -= 25;
             Destroy(collider.gameObject);
         }
         else if (collider.CompareTag("Grill"))
         {
-            Debug.Log("10 s");
             health -= 10;
             Destroy(collider.gameObject);
         }
         else if (collider.CompareTag("IceCube"))
         {
-            Debug.Log("5 s");
             health += 5;
             Destroy(collider.gameObject);
         }
         else if (collider.CompareTag("AC"))
         {
-            Debug.Log("20 s");
-            health += 20;
+            health += 30;
             Destroy(collider.gameObject);
         }
         else if (collider.CompareTag("Popsicle"))
         {
-            Debug.Log("10 s");
             health += 10;
             Destroy(collider.gameObject);
         }
@@ -174,7 +193,7 @@ public class Player : MonoBehaviour
 
     private static float GetDeltaTime()
     {
-        return Time.deltaTime*2;
+        return Time.deltaTime;
     }
 
     void restart_game()
@@ -185,7 +204,6 @@ public class Player : MonoBehaviour
     private void throw_snowball()
     {
         Vector3 oneUnitRightOfMe = this.transform.position + Vector3.right;
-        //animator.SetBool("Is_thrown", true);
         GameObject Snowball = Instantiate(snowball, oneUnitRightOfMe, Quaternion.identity);
         Snowball.GetComponent<Rigidbody2D>().AddForce(new Vector2(12f, 4f), ForceMode2D.Impulse);
     }
---
+++ Final_Project/Assets/Scripts/Sun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{ 
+    private Vector3 offset;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * 6 * Time.deltaTime);
+    }
+}
