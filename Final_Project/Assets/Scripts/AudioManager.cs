ed9e99a567da32783c74e4b630ffdf3824760b49
Julian Sollozo <jsollozo@uci.edu>
2019-03-04 18:13:32 -0800

    Adding Audio Manager
    -audio manager added
    -added more music and sound effects
    -Added Resources File

---
+++ Final_Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance { get; private set; }
+    public AudioSource BGMPlayer, SFXPlayer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlaySFX(string clip)
+    {
+        PlaySFX(clip, Random.Range(0.3f, 1f));
+    }
+
+    public void PlaySFX(string clip, float volume)
+    {
+        AudioClip audioClip = Resources.Load($"Audio/{clip}") as AudioClip;
+        SFXPlayer.PlayOneShot(audioClip, volume);
+    }
+}
